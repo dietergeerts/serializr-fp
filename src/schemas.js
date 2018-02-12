d1bdd4c2b1fb9789e5946d197a05e4b6fad4fb39
dietergeerts <dieter@dworks.be>
2018-02-12 19:49:53 +0100

    Add `DATE_ONLY` and `withDefault` schemas

--- src/schemas.js
+++ src/schemas.js
@@ -1,6 +1,9 @@
 import _assign from 'lodash/fp/assign';
 import _flow from 'lodash/fp/flow';
+import _isNil from 'lodash/fp/isNil';
 import _isUndefined from 'lodash/fp/isUndefined';
+import _placeholder from 'lodash/fp/placeholder';
+import _slice from 'lodash/fp/slice';
 import {deserialize, serialize, SKIP} from "./index";
 
 /**
@@ -18,6 +21,13 @@ export const DATE = {
     serialize: value => value && value.toISOString(),
     deserialize: value => value && new Date(value),
 };
+
+/**
+ * @type {PropertySchema}
+ */
+export const DATE_ONLY = _assign(DATE, {
+    serialize: _flow(DATE.serialize, _slice(0, 10)),
+});
 
 /**
  * @private
@@ -56,12 +66,19 @@ export const alias = (property, schema) => _assign(schema, {property});
 const skipIfUndefined = value => _isUndefined(value) ? SKIP : value;
 
 /**
+ * @private
+ * @param {!PropertyTransformer} transformer
+ * @returns {!PropertyTransformer}
+ */
+const optionalTransformer = _flow(_placeholder, skipIfUndefined);
+
+/**
  * @param {!PropertySchema} schema
  * @returns {!PropertySchema}
  */
 export const optional = schema => ({
-    serialize: _flow(schema.serialize, skipIfUndefined),
-    deserialize: _flow(schema.deserialize, skipIfUndefined),
+    serialize: optionalTransformer(schema.serialize),
+    deserialize: optionalTransformer(schema.deserialize),
 });
 
 /**
@@ -71,4 +88,14 @@ export const optional = schema => ({
 export const computed = compute => ({
     serialize: () => SKIP,
     deserialize: (value, json) => compute(json),
+});
+
+/**
+ * @param {*} defaultValue
+ * @param {!PropertySchema} schema
+ * @returns {!PropertySchema}
+ */
+export const withDefault = (defaultValue, schema) => ({
+    serialize: schema.serialize,
+    deserialize: _flow(schema.deserialize, value => _isNil(value) ? defaultValue : value),
 });
