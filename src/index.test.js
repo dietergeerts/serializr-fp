f01ea45e4803b901c78fe06ee0a00668e551a14d
dietergeerts <dieter@dworks.be>
2018-02-13 22:13:07 +0100

    Add unit test stubs for index/core file

---
+++ src/index.test.js
@@ -0,0 +1,14 @@
+describe('SKIP', () => {
+
+    it(`must be defined as a symbol, for usage of skipping properties.`);
+});
+
+describe('serialize', () => {
+
+    it(`must serialize`);
+});
+
+describe('deserialize', () => {
+
+    it(`must deserialize`);
+});
