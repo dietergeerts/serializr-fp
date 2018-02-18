823ec48bf3b3351717b1dc74be76a23bab06b788
dietergeerts <dieter@dworks.be>
2018-02-18 13:55:08 +0100

    Add eslint config and script

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+        mocha: true,
+    },
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module",
+    },
+    extends: "eslint:recommended",
+};
